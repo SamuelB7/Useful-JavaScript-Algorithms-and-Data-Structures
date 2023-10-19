c84d2323feeb143397bb11d4a7703e213e0515d1
Samuel Belo <belo.samuel@gmail.com>
2023-10-19 19:52:23 -0300

    coding challange: two sum added

---
+++ CodingChallanges/TwoSum.js
@@ -0,0 +1,17 @@
+function twoSum(numbersArray, targetSum) {
+    let result = [];
+    for (let i = 0; i < numbersArray.length - 1; i++) {
+        if (i > 0 && numbersArray[i] === numbersArray[i - 1]) continue;
+        let left = i + 1;
+        while (left < numbersArray.length) {
+            let sum = numbersArray[i] + numbersArray[left];
+            if (sum === targetSum) {
+                result.push(i, left);
+                while (numbersArray[left] === numbersArray[left + 1]) left++;
+                left++;
+            }
+            left++;
+        }
+    }
+    return result;
+}
